c065137994f4d312a01802efe46afad2ecd22cb9
Diego1809 <diegoa-sandovalf@unilibre.edu.co>
2021-08-18 07:20:25 -0500

    inicialización Spring boot

---
+++ src/main/java/com/misiontic/Tareas_MS/TareasMsApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.Tareas_MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misiontic/Tareas_MS/TareasMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.Tareas_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
